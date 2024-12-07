a8211b15cb51e539b9093830dea7078ed9d01c55
yusuShin <tlsdbtn0507@naver.com>
2024-12-07 23:33:53 +0900

    styled : 첫페이지와 포장 페이지 tailwind 적용

--- src/pages/diningChoice.tsx
+++ src/pages/diningChoice.tsx
@@ -2,23 +2,34 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { setOrderInfo } from "../feat/order";
 
-
 const DiningChoice: React.FC = () => {
   const naviate = useNavigate();
 
   const confirmDiningType = (dineType: boolean) => {
-    setOrderInfo("isTakeOut", dineType)
+    setOrderInfo("isTakeOut", dineType);
     naviate("/menus");
   };
 
   return (
-    <>
-      <h1>
+    <div className="flex flex-col items-center justify-center h-500 bg-gray-50 p-4">
+      <h1 className="text-2xl font-bold text-gray-800 mb-8">
         식사 장소를 <br /> 선택해주세요
       </h1>
-      <button onClick={() => confirmDiningType(false)}>매장 식사</button>
-      <button onClick={() => confirmDiningType(true)}>포장</button>
-    </>
+      <div className="flex gap-4">
+        <button
+          className="bg-purple-200 text-gray-800 font-medium py-3 px-6 rounded-full shadow-md hover:bg-purple-300 focus:outline-none"
+          onClick={() => confirmDiningType(false)}
+        >
+          매장 식사
+        </button>
+        <button
+          className="bg-purple-200 text-gray-800 font-medium py-3 px-6 rounded-full shadow-md hover:bg-purple-300 focus:outline-none"
+          onClick={() => confirmDiningType(true)}
+        >
+          포장
+        </button>
+      </div>
+    </div>
   );
 };
 
--- src/pages/start.tsx
+++ src/pages/start.tsx
@@ -16,14 +16,25 @@ const Start: React.FC = () => {
   }, []);
 
   return (
-    <>
-      <h1>start</h1>
-      <button onClick={() => navigateToDiningChoice()}>주문시작하기</button>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 p-4">
+      <div
+        onClick={() => navigateToDiningChoice()}
+        className="w-80 h-40 bg-green-100 rounded-lg flex items-center justify-center shadow-md"
+      >
+        <p className="text-lg font-medium text-gray-800">주문 시작하기</p>
+      </div>
       {/* 로컬스토리지에 { isNewbie: true } 저장해두고 진행*/}
-      <button onClick={() => navigateToDiningChoice("newbie")}>
-        키오스크가 처음이라면 저를 눌러주세요
-      </button>
-    </>
+      <div
+        onClick={() => navigateToDiningChoice("newbie")}
+        className="w-60 h-40 bg-pink-100 rounded-3xl flex items-center justify-center mt-6 shadow-md"
+      >
+        <p className="text-center text-gray-800 text-base font-medium">
+          키오스크가 처음이라면
+          <br />
+          저를 눌러주세요
+        </p>
+      </div>
+    </div>
   );
 };
 
